05830cf37d04d92ec6e1f94d535b27e2bb2e0044
Daniele Calixto <danielecalixtob@gmail.com>
2022-08-14 20:57:48 -0300

    feat: add routes to create and list announcements

--- src/routes/announcement.routes.ts
+++ src/routes/announcement.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express"
+import AnnouncementController from "../controllers/Announcement.controller"
 
 const announcementRouter = Router()
+
+announcementRouter.post("/", AnnouncementController.store)
+announcementRouter.get("/", AnnouncementController.index)
 
 export default announcementRouter
