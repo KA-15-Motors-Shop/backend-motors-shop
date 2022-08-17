1517c07c805a5c088293697822fd7c1f58962c33
almeidadylan <almeidadylan793@gmail.com>
2022-08-17 08:02:59 -0400

    error: docker-compose and update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -31,6 +31,12 @@ export default class UserController {
             account_type
         })
 
+        if (user == "Email Already Exists") {
+            return response.status(409).json({
+                error: "Email Already Exists"
+            })
+        }
+
         return response.status(201).json(user)
     }   
 
@@ -59,31 +65,43 @@ export default class UserController {
 
         const user = await userRepository.delete({ id: id })
 
-        return response.status(204)
+        return response.status(204).json()
     }
 
     static async update(request: Request, response: Response) {
         const { id } = request.params
-        const { name, password, cpf, phone, birth_date, description, account_type } = request.body
+        const { name, email, password, cpf, phone, birth_date, description, account_type } = request.body
 
         const userRepository = AppDataSource.getRepository(User)
 
-        const date = String(new Date)
+        const date = new Date()
 
         const hashedPassword = await bcrypt.hash(password, 10)
+        console.log("hashed password:", hashedPassword)
+    
 
-        const update = await userRepository.update(id, {
-            name: name, 
-            password: hashedPassword, 
-            cpf: cpf, 
-            phone: phone, 
-            birth_date: birth_date, 
-            description: description,
-            account_type: account_type,
+        const oldUser = await userRepository.find( { where: { id: id } } );
+
+        const newUser = {
+            name: name || oldUser[0].name,
+            email: email || oldUser[0].email,
+            cpf: cpf || oldUser[0].cpf,
+            phone: phone || oldUser[0].phone,
+            birth_date: birth_date || oldUser[0].birth_date,
+            description: description || oldUser[0].description,
+            account_type: account_type || oldUser[0].account_type,
+            password: password? (hashedPassword) : (oldUser[0].password),
+            //password: password? hashedPassword || oldUser[0].password
             updated_at: date
-        })
+        }
+        console.log("old user:", oldUser)
+        console.log("new user:", newUser)
+
+        const update = await userRepository.update( id, newUser)
+
 
         const user = await userRepository.find( { where: { id: id } } );
+        console.log("updated user:", user)
 
         return response.json(user)
     }
@@ -91,7 +109,21 @@ export default class UserController {
     static async login(request: Request, response: Response) {
         const { email, password } = request.body
 
-        const userRepository = AppDataSource.getMongoRepository(User)
+        const userRepository = AppDataSource.getRepository(User)
+        const users = await userRepository.find()
+
+        if ( email == undefined) {
+            return response.status(409).json({ error: "missing email"})
+        }
+
+        const account = users.find((user) => user.email === email)
+
+        if ( password == undefined) {
+            return response.status(409).json({ error: "missing password"})
+        }
+        if ( !account || !bcrypt.compareSync(password, account.password) ) {
+            return response.status(409).json({ error: "Wrong email/password"});
+          }
 
         const token = jwt.sign({ email: email}, "SECRET_KEY", { expiresIn: "24h" })
 
--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -4,50 +4,53 @@ import { v4 as uuidv4 } from "uuid";
 import * as bcrypt from "bcrypt";
 
 interface UserDataParams {
-  name: string
-  email: string
-  cpf: string
-  phone: string
-  birth_date: string
-  description: string
-  password: string,
-  account_type: string
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  birth_date: string;
+  description: string;
+  password: string;
+  account_type: string;
 }
 
 export default class CreateUserService {
   async execute(data: UserDataParams) {
-    const userRepository = AppDataSource.getRepository(User)
-    console.log("password:", data.password)
+    const userRepository = AppDataSource.getRepository(User);
+    console.log("password:", data.password);
+    const users = await userRepository.find();
 
-    //const { password } = data
-    console.log(data.password)
+    const emailAlreadyExists = users.find((user) => user.email === data.email);
 
-    const hashedPassword = await bcrypt.hash(data.password, 10)
+    if (emailAlreadyExists) {
+      return "Email Already Exists";
+    }
 
-    const date = "2022-05-01 15:30:01"//String(new Date);
-    const teste = String(new Date)
-    console.log("teste:", teste)
-    console.log("date:", date)
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const date = new Date();
+    const formatDate = date.toISOString().replace("T", "");
+    console.log("date:", date.toISOString().replace("T", " "));
 
     const newUser = {
-        id: uuidv4(),
-        name: data.name,
-        email: data.email,
-        cpf: data.cpf,
-        phone: data.phone,
-        birth_date: data.birth_date,
-        description: data.description,
-        password: hashedPassword,
-        account_type: data.account_type,
-        created_at: date,
-        updated_at: date
-    }
-    console.log(newUser)
+      id: uuidv4(),
+      name: data.name,
+      email: data.email,
+      cpf: data.cpf,
+      phone: data.phone,
+      birth_date: data.birth_date,
+      description: data.description,
+      password: hashedPassword,
+      account_type: (data.account_type = "buyer"),
+      created_at: date,
+      updated_at: date,
+    };
+    console.log(newUser);
 
-    const user = userRepository.create(newUser)
+    const user = userRepository.create(newUser);
 
-    await userRepository.save(user)
+    await userRepository.save(user);
 
-    return user
+    return user;
   }
 }
